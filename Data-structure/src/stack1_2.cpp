0e1ef8754d26d7fcff3fb1a27e4704416a06e93e
LiJianping <1196934490@qq.com>
2020-05-05 18:28:32 +0800

    Update stack1_2.cpp

--- Data-structure/src/stack1_2.cpp
+++ Data-structure/src/stack1_2.cpp
@@ -3,7 +3,7 @@ no exception handling */
 #include <iostream>
 #include <cstddef>
 
-const std::size_t STACK_MAX_SIZE = 100;
+// const std::size_t STACK_MAX_SIZE = 100;
 
 // linked list
 struct Node {
@@ -13,20 +13,82 @@ struct Node {
 
 class Stack {
 public:
-
     void push(int i);
     void pop();
+    Node* prev(Node* p);
 
     std::size_t size();
-    bool empty();
-    bool full() {return sz == STACK_MAX_SIZE ? true : false;}
+    bool isEmpty();
 
 private:
-    Node head;
-    std::size_t sz;
+    Node* head = new Node();
+    Node* tail = head;
+
 };
+inline
+bool Stack::isEmpty()
+{
+    return head->next == nullptr ? true : false;
+}
 
 std::size_t Stack::size()
 {
-    
+    std::size_t sz = 0;
+    for (Node* p = head; p->next != nullptr; p = p->next)
+    {
+        sz++;
+    }
+    return sz;
+}
+
+inline
+void Stack::push(int i)
+{
+    tail->num = i;
+    tail->next = new Node();
+    tail = tail->next;
+}
+
+inline
+void Stack::pop()
+{
+    if (head == tail)
+    {
+        std::cout << "error: attempting to pop an empty stack" << std::endl;
+    }
+    else
+    {
+        delete tail;
+        tail = prev(tail);
+        tail->next = nullptr;
+    }
+}
+
+Node* Stack::prev(Node* p)  // possible issue
+{
+    for (Node* ptr = head; ptr->next != nullptr; ptr = ptr->next)
+    {
+        if (ptr->next == p)
+        {
+            return ptr;
+        }
+    }
+    return nullptr;
+}
+
+
+
+int main()
+{
+    Stack obj1;
+    std::cout << obj1.size() << std::endl;
+    obj1.push(3);
+    obj1.push(4);
+    obj1.push(5);
+    std::cout << obj1.size() << std::endl;
+    obj1.pop();
+    std::cout << obj1.size() << std::endl;
+
+    system("pause");
+    return 0;
 }
