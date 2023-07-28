34e28cc864371e3aa3db628934c11b45d8240fda
Li Jianping <1196934490@qq.com>
2023-07-28 22:44:11 +0800

    element type

---
+++ cpp/cpp-templates/19_traits/element_type.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <list>
+#include <iostream>
+
+
+template <typename Container>
+struct ElementT {
+    using Type = typename Container::value_type;
+};
+
+template <typename T>
+struct ElementT<std::vector<T>> {
+    using Type = T;
+};
+
+template <typename T>
+struct ElementT<std::list<T>> {
+    using Type = T;
+};
+
+template <typename T, size_t N>
+struct ElementT<T[N]> {
+    using Type = T;
+};
+
+template <typename T>
+struct ElementT<T[]> {
+    using Type = T;
+};
+
+template <typename T>
+void printElementType(const T& c) {
+    std::cout << "Container of " << typeid(typename ElementT<T>::Type).name() << " elements.\n";
+}
+
+int main() {
+    std::vector<bool> s;
+    printElementType(s);
+
+    int arr[42];
+    printElementType(arr);
+
+    std::list<double> l;
+    printElementType(l);
+}
