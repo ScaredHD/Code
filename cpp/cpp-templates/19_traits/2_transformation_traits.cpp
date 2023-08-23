875b2d9d439c26c9afced08283945f34b4afd616
Li Jianping <1196934490@qq.com>
2023-08-23 16:31:59 +0800

    decay

--- cpp/cpp-templates/19_traits/2_transformation_traits.cpp
+++ cpp/cpp-templates/19_traits/2_transformation_traits.cpp
@@ -1,4 +1,5 @@
 
+// Removing references
 template <typename T>
 struct RemoveReferenceT {
     using Type = T;
@@ -13,6 +14,24 @@ template <typename T>
 struct RemoveReferenceT<T&&> {
     using Type = T;
 };
+
+// Adding references
+template <typename T>
+struct AddLValueRefT {
+    using Type = T&;
+};
+
+template <typename T>
+using AddLValueRef = typename AddLValueRefT<T>::Type;
+
+template <typename T>
+struct AddRValueRefT {
+    using Type = T&&;
+};
+
+template <typename T>
+using AddRValueRef = typename AddRValueRefT<T>::Type; 
+
 
 int main() {
     RemoveReferenceT<int>::Type t1;       // int
