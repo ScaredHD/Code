7bcd033951da15a5499682f21853d17fccefa851
Li Jianping <1196934490@qq.com>
2024-01-06 09:20:39 +0800

    Leetcode 2506: Similar pairs

---
+++ Leetcode/2506e_similar_pairs.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+class Solution {
+  public:
+    int similarPairs(std::vector<std::string>& words) {
+        std::unordered_map<int, int> rec;
+        for (const auto& word : words) {
+            ++rec[encode(word)];
+        }
+
+        int res{};
+        for (const auto& r : rec) {
+            int n = r.second;
+            if (n > 1) {
+                res += n * (n - 1) / 2;
+            }
+        }
+        return res;
+    }
+
+    int encode(const std::string& str) {
+        int code = 0;
+        for (char ch : str) {
+            int digit = ch - 'a';
+            code |= (1 << digit);
+        }
+        return code;
+    }
+};
+
+int main() {
+    int e1 = Solution().encode("abc");
+    int e2 = Solution().encode("ab");
+    int e3 = Solution().encode("bc");
+    int e4 = Solution().encode("ac");
+    int e5 = Solution().encode("b");
+    int i = 42;
+}
