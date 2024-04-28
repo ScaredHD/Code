6d896e5dc4a0862b9119d113777bac985c27f0c2
Li Jianping <1196934490@qq.com>
2024-04-28 12:19:04 +0800

    sicp 1.3.3

---
+++ SICP/excercises/ch1/1.3/1.3.3 procedures as general methods/fixed-point.rkt
@@ -0,0 +1,24 @@
+#lang sicp
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+        next
+        (try next))))
+  (try first-guess))
+
+(fixed-point cos 1.0)
+
+(fixed-point (lambda (y) (+ (sin y) (cos y))) 1.0)
+
+(define (fp-sqrt x)
+  (fixed-point (lambda (y) (/ (+ y (/ x y)) 2)) 1.0))
+
+(fp-sqrt 2)
+
