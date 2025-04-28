0153aed14bf606571f46d510f126295476043d30
Li Jianping <1196934490@qq.com>
2025-04-28 23:23:30 +0800

    Combine two NFA's.

--- python/video_tool/test.py
+++ python/video_tool/test.py
@@ -1,33 +1,36 @@
 
 import tokenizer as tk
 
-machine = tk.NFA()
+# machine = tk.NFA()
 
-machine.add_states({tk.State(i) for i in range(9)})
-machine.set_start(0)
-machine.set_finals({2, 6, 8})
+# machine.add_states({tk.State(i) for i in range(9)})
+# machine.set_start(0)
+# machine.set_finals({2, 6, 8})
 
-machine.add_transitions([
-    (0, 'eps', {1, 3, 7}),
-    (1, 'a', 2),
-    (7, 'a', 7),
-    (7, 'b', 8),
-    (8, 'b', 8),
-    (3, 'a', 4),
-    (4, 'b', 5),
-    (5, 'b', 6)
-])
+# machine.add_transitions([
+#     (0, 'eps', {1, 3, 7}),
+#     (1, 'a', 2),
+#     (7, 'a', 7),
+#     (7, 'b', 8),
+#     (8, 'b', 8),
+#     (3, 'a', 4),
+#     (4, 'b', 5),
+#     (5, 'b', 6)
+# ])
 
 
-simulation = tk.NFASimulation(machine)
+# simulation = tk.NFASimulation(machine)
 
-simulation.process_input('a')
-print(simulation.state_group_track)
-simulation.process_input('a')
-print(simulation.state_group_track)
-simulation.process_input('b')
-print(simulation.state_group_track)
-simulation.process_input('a')
-print(simulation.state_group_track)
+# simulation.process_input('a')
+# print(simulation.state_group_track)
+# simulation.process_input('a')
+# print(simulation.state_group_track)
+# simulation.process_input('b')
+# print(simulation.state_group_track)
+# simulation.process_input('a')
+# print(simulation.state_group_track)
 
-machine.validate()
+# machine.validate()
+
+nfa = tk.nfa_whitespace()
+pass
--- python/video_tool/tokenizer.py
+++ python/video_tool/tokenizer.py
@@ -1,3 +1,4 @@
+import string
 
 class CharStream:
     def __init__(self, path):
@@ -22,6 +23,19 @@ StateID = str | int
 StateGroup = set[State]
 StateIDGroup = set[StateID]
 
+charset_printable = set(string.printable)
+charset_ws = {' ', '\t', '\n'}
+charset_ns = charset_printable - charset_ws
+
+
+
+def is_epsilon(input: str):
+    return input == '' or input == 'epsilon' or input == 'eps'
+
+
+def is_default(input: str):
+    return input == 'default' or input == 'other'
+
 
 class Transition:
     def __init__(self):
@@ -30,13 +44,13 @@ class Transition:
         self.epsilon: StateIDGroup = set()
 
     def query(self, input: str) -> StateIDGroup:
-        if input == '' or input == 'epsilon' or input == 'ε' or input == 'eps':
+        if is_epsilon(input):
             return self.epsilon
-        if input == 'default':
+        if is_default(input):
             return self.default
         if input in self.on_input:
             return self.on_input[input]
-        return set()
+        return self.default
 
 
 class NFA:
@@ -64,6 +78,12 @@ class NFA:
     def check_state_presence(self, state):
         if not self.contains_state(state):
             raise Exception(f'State ID: {state} not exists')
+        
+    def create_states(self, state_ids : StateIDGroup):
+        self.states = {s_id : State(s_id) for s_id in state_ids}
+        self.transitions: dict[StateID, Transition] = {
+            s: Transition() for s in state_ids}
+        self.start = None
 
     def add_states(self, states: StateGroup):
         for s in states:
@@ -73,34 +93,42 @@ class NFA:
             self.states[s.id] = s
             self.transitions[s.id] = Transition()
 
-    def add_transition(self, from_ids: StateIDGroup | StateID, input: str, to_ids: StateIDGroup | StateID):
+    def add_transition(self, from_ids: StateIDGroup | StateID, inputs: set[str] | str, to_ids: StateIDGroup | StateID):
         from_set = set()
         if type(from_ids) is str or type(from_ids) is int:
             from_set = {from_ids}
         else:
             from_set = from_ids
-        
+
+        for from_id in from_set:
+            self.check_state_presence(from_id)
+
         to_set = set()
         if type(to_ids) is str or type(to_ids) is int:
             to_set = {to_ids}
         else:
             to_set = to_ids
 
-        for from_id in from_set:
-            self.check_state_presence(from_id)
         for to_id in to_set:
             self.check_state_presence(to_id)
 
+        input_set = set()
+        if type(inputs) is str:
+            input_set.add(inputs)
+        else:
+            inputs_set = inputs
+
         for from_id in from_set:
-            t = self.transitions[from_id]
-            if input == '' or input == 'epsilon' or input == 'ε' or input == 'eps':
-                t.epsilon = t.epsilon.union(to_set)
-            elif input == 'default':
-                t.default = t.default.union(to_set)
-            elif input in t.on_input:
-                t.on_input[input] = t.on_input[input].union(to_set)
-            else:
-                t.on_input[input] = to_set
+            for input in input_set:
+                t = self.transitions[from_id]
+                if is_epsilon(input):
+                    t.epsilon = t.epsilon.union(to_set)
+                elif is_default(input):
+                    t.default = t.default.union(to_set)
+                elif input in t.on_input:
+                    t.on_input[input] = t.on_input[input].union(to_set)
+                else:
+                    t.on_input[input] = to_set
 
     def add_transitions(self, transition_list: list[tuple]):
         for from_ids, input, to_ids in transition_list:
@@ -118,6 +146,21 @@ class NFA:
 
         if not exist_final:
             raise Exception('There must be at least one final state')
+
+
+def combine_nfa(nfa0: NFA, nfa1: NFA, new_start: StateID):
+    states0 = nfa0.states.keys()
+    states1 = nfa1.states.keys()
+    combined_states = states0 | states1
+    assert len(combined_states) == len(states0) + \
+        len(states1), "There exist states with same id's"
+
+    res = NFA()
+    res.states = combined_states
+    res.transitions = nfa0.transitions | nfa1.transitions
+    res.start = new_start
+
+    return res
 
 
 class NFASimulation:
@@ -158,6 +201,24 @@ class NFASimulation:
             after = self.query_move(before, 'epsilon') | state_ids
 
         return after
+
+
+def nfa_number():
+    nfa = NFA()
+    # TODO:
+
+
+def nfa_string():
+    pass
+
+
+def nfa_whitespace():
+    nfa = NFA()
+    nfa.create_states({'ws0', 'ws1'})
+    nfa.set_finals({'ws1'})
+    nfa.set_start('ws0')
+    nfa.add_transitions([('ws0', charset_ws, 'ws0'), ('ws0', 'default', 'ws1')])
+    return nfa
 
 
 class Tokenizer:
