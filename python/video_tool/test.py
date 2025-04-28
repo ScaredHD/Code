50e824b5c056fd8ed96eaac98cfc80a6aea26191
Li Jianping <1196934490@qq.com>
2025-04-28 20:43:53 +0800

    NFASimulation with move and epsilon closure.

--- python/video_tool/test.py
+++ python/video_tool/test.py
@@ -3,11 +3,31 @@ import tokenizer as tk
 
 machine = tk.NFA()
 
-machine.add_states({tk.State(0), tk.State(1),
-                   tk.State(2), tk.State(3, True)})
-
-machine.add_transition(0, 'c', 1)
-
+machine.add_states({tk.State(i) for i in range(9)})
 machine.set_start(0)
+machine.set_finals({2, 6, 8})
+
+machine.add_transitions([
+    (0, 'eps', {1, 3, 7}),
+    (1, 'a', 2),
+    (7, 'a', 7),
+    (7, 'b', 8),
+    (8, 'b', 8),
+    (3, 'a', 4),
+    (4, 'b', 5),
+    (5, 'b', 6)
+])
+
+
+simulation = tk.NFASimulation(machine)
+
+simulation.process_input('a')
+print(simulation.state_group_track)
+simulation.process_input('a')
+print(simulation.state_group_track)
+simulation.process_input('b')
+print(simulation.state_group_track)
+simulation.process_input('a')
+print(simulation.state_group_track)
 
 machine.validate()
--- python/video_tool/tokenizer.py
+++ python/video_tool/tokenizer.py
@@ -18,21 +18,45 @@ class State:
         self.is_final = is_final
 
 
+StateID = str | int
+StateGroup = set[State]
+StateIDGroup = set[StateID]
+
+
 class Transition:
-    def __init__(self, *, default_to_state=None, on_input_dict={}):
-        self.default: str | int = default_to_state
-        self.on_input: dict[str, set[str | int]] = on_input_dict
+    def __init__(self):
+        self.default: StateIDGroup = set()
+        self.on_input: dict[str, StateIDGroup] = {}
+        self.epsilon: StateIDGroup = set()
+
+    def query(self, input: str) -> StateIDGroup:
+        if input == '' or input == 'epsilon' or input == 'ε' or input == 'eps':
+            return self.epsilon
+        if input == 'default':
+            return self.default
+        if input in self.on_input:
+            return self.on_input[input]
+        return set()
 
 
 class NFA:
-    def __init__(self, states: set[State] = set()):
-        self.states: dict[int | str, State] = {s.id: s for s in states}
-        self.transition: dict[int | str, Transition] = {
+    def __init__(self, states: StateGroup = set()):
+        self.states: dict[StateID, State] = {s.id: s for s in states}
+        self.transitions: dict[StateID, Transition] = {
             s.id: Transition() for s in states}
         self.start = None
 
     def set_start(self, id):
         self.start = id
+
+    def set_finals(self, ids: StateIDGroup):
+        for id in ids:
+            if id not in self.states:
+                raise Exception(f'State ID: {id} not exists')
+            self.states[id].is_final = True
+
+    def final_states(self) -> StateIDGroup:
+        return {s.id for s in self.states.values() if s.is_final}
 
     def contains_state(self, id):
         return id in self.states
@@ -41,28 +65,46 @@ class NFA:
         if not self.contains_state(state):
             raise Exception(f'State ID: {state} not exists')
 
-    def add_states(self, states: set[State]):
+    def add_states(self, states: StateGroup):
         for s in states:
             if s.id in self.states:
                 raise Exception(f'id: {s.id} already exists')
 
             self.states[s.id] = s
-            self.transition[s.id] = Transition()
+            self.transitions[s.id] = Transition()
 
-    def set_default_transition(self, from_id, to_id):
-        self.check_state_presence(from_id)
-        self.check_state_presence(to_id)
-        t = self.transition[from_id]
-        t.default = to_id
-
-    def add_transition(self, from_id, input, to_id):
-        self.check_state_presence(from_id)
-        self.check_state_presence(to_id)
-        t = self.transition[from_id]
-        if input in t.on_input:
-            t.on_input[input].add(to_id)
+    def add_transition(self, from_ids: StateIDGroup | StateID, input: str, to_ids: StateIDGroup | StateID):
+        from_set = set()
+        if type(from_ids) is str or type(from_ids) is int:
+            from_set = {from_ids}
         else:
-            t.on_input[input] = {to_id}
+            from_set = from_ids
+        
+        to_set = set()
+        if type(to_ids) is str or type(to_ids) is int:
+            to_set = {to_ids}
+        else:
+            to_set = to_ids
+
+        for from_id in from_set:
+            self.check_state_presence(from_id)
+        for to_id in to_set:
+            self.check_state_presence(to_id)
+
+        for from_id in from_set:
+            t = self.transitions[from_id]
+            if input == '' or input == 'epsilon' or input == 'ε' or input == 'eps':
+                t.epsilon = t.epsilon.union(to_set)
+            elif input == 'default':
+                t.default = t.default.union(to_set)
+            elif input in t.on_input:
+                t.on_input[input] = t.on_input[input].union(to_set)
+            else:
+                t.on_input[input] = to_set
+
+    def add_transitions(self, transition_list: list[tuple]):
+        for from_ids, input, to_ids in transition_list:
+            self.add_transition(from_ids, input, to_ids)
 
     def validate(self):
         if self.start is None:
@@ -79,13 +121,43 @@ class NFA:
 
 
 class NFASimulation:
-    def __init__(self):
-        self.nfa = NFA()
-        self.state_group_track = []
-        self.input_track = []
+    def __init__(self, nfa: NFA):
+        self.nfa = nfa
+        self.state_group_track = [self.epsilon_closure({self.nfa.start})]
+        self.input_track = ['start']
 
-    def move(self, input) -> set[State]:
-        pass
+    def query_move(self, state_ids: StateIDGroup, input: str) -> StateIDGroup:
+        assert state_ids & self.nfa.states.keys(
+        ) == state_ids, 'state_ids contains invalid state id(s)'
+
+        res = set()
+        for state_id in state_ids:
+            t = self.nfa.transitions[state_id]
+            to_states = t.query(input)
+            res = res.union(to_states)
+
+        return res
+
+    def process_input(self, input: str) -> StateIDGroup:
+        assert len(self.state_group_track) == len(
+            self.input_track), 'state_group_track and input_track must be same length'
+        current_states = self.state_group_track[-1]
+        states = self.query_move(current_states, input)
+        states = self.epsilon_closure(states)
+        self.state_group_track.append(states)
+        self.input_track.append(input)
+
+    def epsilon_closure(self, state_ids: StateIDGroup) -> StateIDGroup:
+        assert state_ids & self.nfa.states.keys(
+        ) == state_ids, 'state_ids contains invalid state id(s)'
+
+        before = set()
+        after = state_ids
+        while before != after:
+            before = after
+            after = self.query_move(before, 'epsilon') | state_ids
+
+        return after
 
 
 class Tokenizer:
