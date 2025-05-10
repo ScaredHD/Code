f9f56d36f2d0479ae8e37257ee01f211682b11a2
Li Jianping <1196934490@qq.com>
2025-05-10 20:47:53 +0800

    Remove MyTestRender and associated shader files

--- temp/MyTestRender.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "SceneRendering.h"
-#include "RenderGraphBuilder.h"
-#include "ScreenPass.h"
-
-void AddMyTestPass(FRDGBuilder& GraphBuilder, const FViewInfo& View, FScreenPassTexture SceneColor, const FVector4f& SimpleColor);
